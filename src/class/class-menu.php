6fc3e7937af44745b2c146e85d155ae6bf469f12
Benjamin Lu <benlumia007@gmail.com>
2019-04-18 16:10:09 -0700

    initial commit

---
+++ src/class/class-menu.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Backdrop Core ( framework.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Framework;
+
+/**
+ * Regiser Menu Class
+ */
+class Menu {
+	/**
+	 * Menu
+	 *
+	 * @var string
+	 */
+	public $menu_id;
+
+	public function __construct( $menu_id = array() ) {
+		$this->menu_id = array_merge(
+			$menu_id,
+			$this->default_menus()
+		);
+		$this->register_nav_menus_init();
+	}
+
+	/**
+	 * Default Menus
+	 */
+	public function default_menus() {
+		return array(
+			'primary' => esc_html__( 'Primary Navigation', 'backdrop' ),
+			'social'  => esc_html__( 'Social Navigation', 'backdrop' ),
+		);
+	}
+
+	/**
+	 * Register Custom Menus Initialize
+	 */
+	public function register_nav_menus_init() {
+		add_action( 'after_setup_theme', array( $this, 'register_nav_menus' ) );
+	}
+
+	/**
+	 * Register Custom Menus
+	 */
+	public function register_nav_menus() {
+		foreach ( $this->menu_id as $key => $value ) {
+			$this->create_menus( $value, $key );
+		}
+	}
+
+	/**
+	 * Create Menus
+	 *
+	 * @param string $name outputs name.
+	 */
+	public function create_menus( $name, $id ) {
+		$args = array(
+			$id => $name,
+		);
+		register_nav_menus( $args );
+	}
+}
