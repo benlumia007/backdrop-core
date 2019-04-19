12adc877a486f051273763482f852c173b4a211d
Benjamin Lu <benlumia007@gmail.com>
2019-04-19 11:05:55 -0700

    initial commit

---
+++ src/Customize/CustomizeAbstract.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Backdrop Core ( framework.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Customize;
+
+/**
+ * Regiser Menu Class
+ */
+abstract class CustomizeAbstract implements CustomizeInterface {
+	/**
+	 * Loads Construct
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'customize_register', array( $this, 'register_panels' ) );
+		add_action( 'customize_register', array( $this, 'register_sections' ) );
+		add_action( 'customize_register', array( $this, 'register_settings' ) );
+		add_action( 'customize_register', array( $this, 'register_controls' ) );
+	}
+
+
+	/**
+	 * Register register_panels
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $manager customizer object.
+	 */
+	public function register_panels( $manager ) {}
+
+	/**
+	 * Register register_sections
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $manager customizer object.
+	 */
+	public function register_sections( $manager ) {}
+
+	/**
+	 * Register register_settings
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $manager customizer object.
+	 */
+	public function register_settings( $manager ) {}
+
+	/**
+	 * Register register_controls
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $manager customizer object.
+	 */
+	public function register_controls( $manager ) {}
+}
