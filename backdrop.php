2831cbba1ff21cfe470b5f4c11bdb1f8ef4ee237
Benjamin Lu <benlumia007@gmail.com>
2019-04-20 17:37:39 -0700

    no longer need

--- backdrop.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Backdrop core ( backdrop.php )
- *
- * @package      Backdrop Core
- * @copyright    Copyright (C) 2019. Benjamin Lu
- * @license      GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author       Benjamin Lu ( https://benjlu.com )
- */
