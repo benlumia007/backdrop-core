f23d7e8f110ada1b08f43a23ec8b2c016c60b4d7
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 22:55:05 -0700

    removed theme_support();

--- src/Framework.php
+++ src/Framework.php
@@ -66,40 +66,7 @@ class Framework {
 	 * @return void
 	 */
 	public function theme_setup() {
-		add_action( 'after_setup_theme', array( $this, 'theme_support' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'theme_enqueue' ) );
-	}
-
-	/**
-	 * Loads Default and Register Features
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function theme_support() {
-		/**
-		 * By adding add_theme_support( 'title-tag' );, this will let WordPress manage all document titles and should be use instead of wp_title();.
-		 */
-		add_theme_support( 'title-tag' );
-		/**
-		 * By adding add_theme_support( 'automatic-feed-links' );, this feature when enabled allows feed links for post and comments
-		 * in the head of the theme. This feature should be used in place of of the deprecated function automatic_feed_links();.
-		 */
-		add_theme_support( 'automatic-feed-links' );
-		/**
-		 * By adding add_theme_support( 'html5', arrayy() );, this feature when enabled allows the user use of HTML5 markup for
-		 * comment list, comment forms, search forms, galleries, and captions.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'comment-list',
-				'comment-form',
-				'search-form',
-				'gallery',
-				'caption',
-			)
-		);
 	}
 
 	/**
@@ -118,12 +85,6 @@ class Framework {
 		 * load font awesome 5.0 into one css file.
 		 */
 		wp_enqueue_style( 'backdrop-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/css/custom-fonts.css' ), array(), '1.0.0' );
-		/**
-		 * This allows users to comment by clicking on reply so that it gets nested.
-		 */
-		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
-		}
 	}
 
 	/**
