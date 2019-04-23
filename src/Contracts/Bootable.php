abaa35d964c4b45ffdeda73645ba28557d6351fa
Benjamin Lu <benlumia007@gmail.com>
2019-04-23 09:27:33 -0700

    interface Bootable

---
+++ src/Contracts/Bootable.php
@@ -0,0 +1,21 @@
+<?php //phpcs:ignore
+/**
+ * Backdrop Core ( framework.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Contracts;
+
+interface Bootable {
+	/**
+	 * Boots Framework.
+	 */
+	public function boot();
+}
