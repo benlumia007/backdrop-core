aa8c90c000b15adf8fd48572cf76bf497dd38fca
Benjamin Lu <benlumia007@gmail.com>
2019-04-23 13:38:59 -0700

    refed wrap centered images

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -148,7 +148,4 @@
 		}
 		position = $(this).scrollTop();
 	});
-
-	// Wrap centered images in a new figure element
-	$( 'img.aligncenter' ).wrap( '<figure class="centered-image"></figure>');
 } )( jQuery );
