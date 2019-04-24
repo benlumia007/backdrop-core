8b9e56495fb8268c56661763313d1d37e30cb0ea
Benjamin Lu <benlumia007@gmail.com>
2019-04-23 17:30:23 -0700

    updated

---
+++ src/View/View.php
@@ -0,0 +1,23 @@
+<?php // phpcs:ignore
+/**
+ * Backdrop Core ( framework.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\View;
+
+use Benlumia007\Backdrop\Contracts\Displayable;
+
+/**
+ * Regiser Menu Class
+ */
+class View extends Displayable  {
+	public function display( $type, $items = [] ) {}
+}
