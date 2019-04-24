b8760998e78b712538bf081aadb2ab0cdcfc1066
Benjamin Lu <benlumia007@gmail.com>
2019-04-23 18:54:11 -0700

    updated and added functions-view.php

---
+++ src/View/functions-view.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Backdrop Core ( functions-entry.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\View;
+
+function display( $type, $items = [] ) {
+	
+}
