3efb4534e66b847d2c1feb14bfe1199e7d275e2e
Benjamin Lu <benlumia007@gmail.com>
2019-04-24 00:10:46 -0700

    initial commit

---
+++ src/Contracts/Renderable.php
@@ -0,0 +1,22 @@
+<?php // phpcs:ignore
+/**
+ * Backdrop Core ( Displayable.php )
+ *
+ * @package     Backdrop Core
+ * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+namespace Benlumia007\Backdrop\Contracts;
+
+interface Renderable {
+	/**
+	 * Displayable
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public static function render();
+}
