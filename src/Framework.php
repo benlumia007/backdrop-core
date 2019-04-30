a8eabb4d6cdfab07b68f949ed406b669f5e85aed
Benjamin Lu <benlumia007@gmail.com>
2019-04-30 08:00:45 -0700

    updated

--- src/Framework.php
+++ src/Framework.php
@@ -30,7 +30,12 @@ class Framework {
 	 * Private Constructor
 	 */
 	private function __construct() {
-		$this->boot();
+		self::$instance = $this;
+		$this->theme_setup();
+		$this->load_menu();
+		$this->load_sidebar();
+		$this->load_layout();
+		$this->load_admin_page();
 	}
 
 	/**
@@ -41,21 +46,6 @@ class Framework {
 			new self();
 		}
 		return self::$instance;
-	}
-
-	/**
-	 * Loads Default and Register Features
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function boot() {
-		self::$instance = $this;
-		$this->theme_setup();
-		$this->load_menu();
-		$this->load_sidebar();
-		$this->load_layout();
-		$this->load_admin_page();
 	}
 
 	/**
