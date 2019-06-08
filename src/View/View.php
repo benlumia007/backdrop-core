23b4a8b99e321d28ed8e9d1ae5be354de764c03c
Benjamin Lu <benlumia007@users.noreply.github.com>
2019-06-08 12:32:11 -0700

    update fix displays of menus
    update fix displays of menus

--- src/View/View.php
+++ src/View/View.php
@@ -49,42 +49,45 @@ class View implements Viewable {
  * @access public
  * @return void
  */
-function menu_display() {
-	if ( has_nav_menu( 'primary' ) ) { ?>
-		<div class="main-navigation">
-			<nav id="site-navigation" class="primary-navigation">
-				<button class="menu-toggle" aria-conrol="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'backdrop-core' ); ?></button>
+function menu_display( $item ) {
+	if ( 'primary' === $item ) {
+		if ( has_nav_menu( 'primary' ) ) { ?>
+			<div class="main-navigation">
+				<nav id="site-navigation" class="primary-navigation">
+					<button class="menu-toggle" aria-conrol="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'backdrop-core' ); ?></button>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'primary',
+								'menu_id'        => 'primary-menu',
+								'menu_class'     => 'nav-menu',
+								'depth'			 => 2,
+							)
+						);
+					?>
+				</nav>
+			</div>
+		<?php }
+	} elseif ( 'social' === $item ) {
+		if ( has_nav_menu( 'social' ) ) { ?>
+			<nav id="site-social" class="site-social">
 				<?php
 					wp_nav_menu(
 						array(
-							'theme_location' => 'primary',
-							'menu_id'        => 'primary-menu',
-							'menu_class'     => 'nav-menu',
-							'depth'			 => 2,
+							'theme_location'  => 'social',
+							'container'       => 'nav',
+							'container_id'    => 'menu-social',
+							'container_class' => 'menu-social',
+							'menu_id'         => 'menu-social-items',
+							'menu_class'      => 'menu-items',
+							'depth'           => 1,
+							'link_before'     => '<span class="screen-reader-text">',
+							'link_after'      => '</span>',
+							'fallback_cb'     => '',
 						)
 					);
 				?>
 			</nav>
-		</div>
-	<?php } elseif ( has_nav_menu( 'social' ) ) { ?>
-		<nav id="site-social" class="site-social">
-			<?php
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'social',
-						'container'       => 'nav',
-						'container_id'    => 'menu-social',
-						'container_class' => 'menu-social',
-						'menu_id'         => 'menu-social-items',
-						'menu_class'      => 'menu-items',
-						'depth'           => 1,
-						'link_before'     => '<span class="screen-reader-text">',
-						'link_after'      => '</span>',
-						'fallback_cb'     => '',
-					)
-				);
-			?>
-		</nav>
-		<?php
+		<?php }
 	}
 }
