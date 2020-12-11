e5566322e895be321d58c90f485ad4cb0efb257f
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-10 20:13:54 -0800

    update file

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -23,20 +23,64 @@
  *  1.0 - jQuery - Navigation
  * ************************************************************************************************************************
  */
-( function( $ ) {
-	var container, button, menu, links, subMenus;
+( function() {
+	var container, button, dropdown, icon, screenreadertext, parentLink, menu, submenu, links, i, len;
 
-	container = document.getElementById('site-navigation');
-	if (!container) {
+	container = document.getElementById( 'header' );
+	if ( ! container ) {
 		return;
 	}
 
-	button = container.getElementsByTagName('button')[0];
-	if ('undefined' === typeof button) {
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button ) {
 		return;
 	}
 
-	menu = container.getElementsByTagName('ul')[0];
+	menu = container.getElementsByTagName( 'nav' )[0];
+
+	screenreadertext = document.createElement( 'span' );
+	screenreadertext.classList.add( 'screen-reader-text' );
+	screenreadertext.textContent = backdropCoreScreenReaderText.expandMain;
+	button.appendChild( screenreadertext );
+
+	parentLink = container.querySelectorAll( '.menu-item-has-children, .page_item_has_children' );
+
+	for ( i = 0, len = parentLink.length; i < len; i++ ) {
+		var dropdown = document.createElement( 'button' ),
+			submenu = parentLink[i].querySelector( '.sub-menu' ),
+			icon = document.createElement( 'span' ),
+			screenreadertext = document.createElement( 'span' );
+
+		icon.classList.add( 'dashicons' );
+		icon.setAttribute( 'aria-hidden', 'true' );
+
+		screenreadertext.classList.add( 'screen-reader-text' );
+		screenreadertext.textContent = backdropCoreScreenReaderText.expandChild;
+
+		parentLink[i].insertBefore( dropdown, submenu );
+		dropdown.classList.add( 'dropdown-toggle' );
+		dropdown.setAttribute( 'aria-expanded', 'false' );
+		dropdown.appendChild( icon );
+		dropdown.appendChild( screenreadertext );
+
+		dropdown.onclick = function() {
+			var parentLink = this.parentElement,
+				submenu = parentLink.querySelector( '.sub-menu' ),
+				screenreadertext = this.querySelector( '.screen-reader-text' );
+
+			if ( -1 !== parentLink.className.indexOf( 'toggled-on' ) ) {
+				parentLink.className = parentLink.className.replace( ' toggled-on', '' );
+				this.setAttribute( 'aria-expanded', 'false' );
+				submenu.setAttribute ( 'aria-expanded', 'false');
+				screenreadertext.textContent = backdropCoreScreenReaderText.expandChild;
+			} else {
+				parentLink.className += ' toggled-on';
+				this.setAttribute( 'aria-expanded', 'true' );
+				submenu.setAttribute ( 'aria-expanded', 'true');
+				screenreadertext.textContent = backdropCoreScreenReaderText.collapseChild;
+			}
+		};
+	}
 
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
@@ -44,36 +88,34 @@
 		return;
 	}
 
-	menu.setAttribute('aria-expanded', 'false');
-	if (-1 === menu.className.indexOf('nav-menu')) {
-		menu.className += 'nav-menu';
-	}
+	menu.setAttribute( 'aria-expanded', 'false' );
+		if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+			menu.className += ' nav-menu';
+		}
 
 	button.onclick = function() {
-		if (-1 !== container.className.indexOf('toggled')) {
-			container.className = container.className.replace(' toggled', '');
-			button.setAttribute('aria-expanded', 'false');
-			menu.setAttribute('aria-expanded', 'false');
+		screenreadertext = this.querySelector( '.screen-reader-text' );
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.setAttribute( 'aria-expanded', 'false' );
+			screenreadertext.textContent = backdropCoreScreenReaderText.expandMain;
+			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
 			container.className += ' toggled';
-			button.setAttribute('aria-expanded', 'true');
-			menu.setAttribute('aria-expanded', 'true');
+			button.setAttribute( 'aria-expanded', 'true' );
+			screenreadertext.textContent = backdropCoreScreenReaderText.collapseMain;
+			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
 
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName('a');
-	subMenus = menu.getElementsByTagName('ul');
+	// Get all the link elements within the primary menu.
 
-	// Set menu items with submenus to aria-haspopup="true".
-	for (var i = 0, len = subMenus.length; i < len; i++) {
-		subMenus[i].parentNode.setAttribute('aria-haspopup', 'true');
-	}
+	links = menu.getElementsByTagName( 'a' );
 
 	// Each time a menu link is focused or blurred, toggle focus.
-	for (i = 0, len = links.length; i < len; i++) {
-		links[i].addEventListener('focus', toggleFocus, true);
-		links[i].addEventListener('blur', toggleFocus, true);
+	for ( i = 0, len = links.length; i < len; i++ ) {
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
 	}
 
 	/**
@@ -83,69 +125,49 @@
 		var self = this;
 
 		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while (-1 === self.className.indexOf('nav-menu')) {
+		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
 
 			// On li elements toggle the class .focus.
-			if ('li' === self.tagName.toLowerCase()) {
-				if (-1 !== self.className.indexOf('focus')) {
-					self.className = self.className.replace('focus', '');
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
 				} else {
-					self.className += 'focus';
+					self.className += ' focus';
 				}
 			}
-
 			self = self.parentElement;
 		}
 	}
 
-	function initMainNavigation(container) {
-		// Add dropdown toggle that display child menu items.
-		container.find('.menu-item-has-children > a, .page_item_has_children > a').after('<button class="dropdown-toggle" aria-expanded="false">' + backdropCoreScreenReaderText.expand + '</button>');
+	/**
+	 * Toggles `focus` class to allow submenu access on tablets.
+	 */
+	( function( container ) {
+		var touchStartFn, i,
+			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
-		container.find('.dropdown-toggle').click(function(e) {
-			var _this = $(this);
-			e.preventDefault();
-			_this.toggleClass('toggle-on');
-			_this.next('.children, .sub-menu').toggleClass('toggled-on');
-			_this.attr('aria-expanded', _this.attr('aria-expanded') === 'false' ? 'true' : 'false');
-			_this.html(_this.html() === backdropCoreScreenReaderText.expand ? backdropCoreScreenReaderText.collapse : backdropCoreScreenReaderText.expand);
-		});
-	}
-	initMainNavigation( $('.primary-navigation'));
+		if ( 'ontouchstart' in window ) {
+			touchStartFn = function( e ) {
+				var menuItem = this.parentNode, i;
 
-	// Re-initialize the main navigation when it is updated, persisting any existing submenu expanded states.
-	$(document ).on('customize-preview-menu-refreshed', function(e, params) {
-		if ('primary-navigation' === params.wpNavMenuArgs.theme_location) {
-			initMainNavigation( params.newContainer );
+				if ( ! menuItem.classList.contains( 'focus' ) ) {
+					e.preventDefault();
+					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if ( menuItem === menuItem.parentNode.children[i] ) {
+							continue;
+						}
+						menuItem.parentNode.children[i].classList.remove( 'focus' );
+					}
+					menuItem.classList.add( 'focus' );
+				} else {
+					menuItem.classList.remove( 'focus' );
+				}
+			};
 
-			// Re-sync expanded states from oldContainer.
-			params.oldContainer.find('.dropdown-toggle.toggle-on').each(function() {
-				var containerId = $(this).parent().prop('id');
-				$(params.newContainer).find('#' + containerId + ' > .dropdown-toggle').triggerHandler('click');
-			});
-		}
-	});
-
-	// Hide/show toggle button on scroll
-
-	var position, direction, previous;
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() >= position ){
-			direction = 'down';
-			if(direction !== previous){
-				$('.menu-toggle').addClass('hide');
-
-				previous = direction;
-			}
-		} else {
-			direction = 'up';
-			if(direction !== previous){
-				$('.menu-toggle').removeClass('hide');
-
-				previous = direction;
+			for ( i = 0; i < parentLink.length; ++i ) {
+				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
 			}
 		}
-		position = $(this).scrollTop();
-	});
-} )( jQuery );
+	}( container ) );
+} )();
