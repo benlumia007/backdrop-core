e040c14c00625ccb0b838b3d8998dcd0d962e0e7
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-01-26 19:04:48 -0800

    src/Contracts/Navigation/Menu.php

---
+++ src/Contracts/Navigation/Menu.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Backdrop Core ( src/Contracts/Menu/Menu.php )
+ *
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Contracts\Navigation;
+use Benlumia007\Backdrop\Contracts\Bootable;
+
+/**
+ * Menu Interface
+ *
+ * @since  2.0.0
+ * @access public
+ *
+ * @link   ( https://developer.wordpress.org/themes/customize-api )
+ */
+interface Menu extends Bootable {
+	/**
+	 * Construct
+	 * 
+	 * @since  2.0.0
+	 * @access public
+	 */
+	public function __construct( $menu_id = [] );
+
+	/**
+	 * Register Menus
+	 * 
+	 * @since  2.0.0
+	 * @access public
+	 */
+	public function register();
+	
+	/**
+	 * Create Menus
+	 * 
+	 * @since  2.0.0
+	 * @access public
+	 */
+	public function create( $name, $id );
+}
