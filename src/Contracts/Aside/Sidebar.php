1cdaff34b885692cf76f93a744a7b7fa4bbde724
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-01-26 19:09:37 -0800

    src/Contracts/Aside/Sidebar.php

---
+++ src/Contracts/Aside/Sidebar.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Backdrop Core ( src/Contracts/Sidebar/Sidebar.php )
+ *
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Contracts\Aside;
+use Benlumia007\Backdrop\Contracts\Bootable;
+
+/**
+ * Sidebar Interface
+ *
+ * @since  1.0.0
+ * @access public
+ */
+interface Sidebar extends Bootable {
+	/**
+	 * Construct
+	 */
+	public function __construct( $sidebar_id = [] );
+
+	/**
+	 * Register register_panels
+	 */
+	public function register();
+	
+	/**
+	 * Register register_sections
+	 */
+	public function create( $name, $id, $desc );
+}
